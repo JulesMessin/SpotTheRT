f0f0c58369e3b6ce56c82dcc6caa5affe668c385
Bastien <bastien.avignon@etu.univ-poitiers.fr>
2025-11-12 09:54:38 +0000

    MàJ controlleur du jeu

--- spot_the_rt/server/controller/spot_the_rt.py
+++ spot_the_rt/server/controller/spot_the_rt.py
@@ -9,25 +9,45 @@ class SpotTheRT:
         self.__nb_game = nb_game
 
 
-    def _add_game(self, new_game_input):
+    def _add_game(self, new_game_input:object):
         self.__list_game.append(new_game_input)
 
     def _get_list_game(self):
         return self.__list_game
 
-    def _set_nb_game(self, new_nb_game):
+    def _set_nb_game(self, new_nb_game:int):
         self.__nb_game = new_nb_game
 
     def _get_nb_game(self):
         return self.__nb_game
 
-    def _is_in_list_game(self, lobby_name_input):
-        if lobby_name_input in self._get_list_game:
+    def _is_in_list_game(self, lobby_name_input:str):
+        id_game = -1
+        correct = False
+        while not correct and id_game <= len(self._get_nb_game()):
+            id_game += 1
+            selected_game_name = self._get_list_game()[id_game]._get_lobby_name()
+            if lobby_name_input == selected_game_name:
+                correct = True
+        if correct:
             return True
         else:
             return False
 
-    def create_lobby_request(self, lobby_name_input, player_pseudo_input):
+    def _is_player_existing(self, player_name_input:str):
+        player_id = -1
+        correct = False
+        while not correct and player_id <= selected_game._get_nb_connected_player():
+            player_id += 1
+            selected_player = selected_game._get_connected_player()[player_id]._get_name_player()
+            if player_pseudo_input == selected_player:
+                correct = True
+        if correct:
+            return True
+        else:
+            return False
+
+    def create_lobby_request(self, lobby_name_input:str, player_pseudo_input:str):
         if not self._is_in_list_game(lobby_name_input):
             new_game_id = self._get_nb_game() + 1
             new_game = Game(lobby_name=lobby_name_input, game_id=new_game_id)
@@ -61,24 +81,28 @@ class SpotTheRT:
         if self._is_in_list_game(lobby_name_input):
             game_id = self._get_list_game().index(lobby_name_input)
             selected_game = self._get_list_game()[game_id]
-            player_id = -1
-            correct = False
-            while not correct and player_id <= selected_game._get_nb_connected_player():
-                player_id += 1
-                selected_player = selected_game._get_connected_player()[player_id]._get_name_player()
-                if player_pseudo_input == selected_player:
-                    correct = True
-            if correct:
+            if self._is_player_existing(player_pseudo_input):
                 selected_game.remove_player_to_lobby(selected_player)
                 """ ENVOYER '4' (requête quitter une partie reussit) AU DEMANDEUR """            
         else:
             """ ENVOYER '9' (requête quitter une partie echouée) AU DEMANDEUR """            
 
-    def is_symbol_correct_request():
-        pass
+    def is_symbol_correct_request(self, player_pseudo_input, lobby_name_input, symbol_input):
+        if self._is_in_list_game(lobby_name_input):
+            game_id = self._get_list_game().index(lobby_name_input)
+            selected_game = self._get_list_game()[game_id]
+            selected_common_card = selected_game._get_game_deck()._get_card(0)
+            try:
+                selected_symbol = selected_common_card.index(symbol_input)
+                """ ENVOYER '50' (requête verif self symbole bon) AU DEMANDEUR """
+                """ ENVOYER '52' (requête verif symbole adversaire bon) AUX AUTRES """
+            except ValueError:
+                """ ENVOYER '51' (requête verif self symbole mauvais) AU DEMANDEUR """
+                pass
 
 def main():
-    pass
+    running_app = SpotTheRT()
+
 
 if __name__=='__main__':
     main()
