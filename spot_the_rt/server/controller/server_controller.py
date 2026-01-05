64a929e4165f425ea07a7ced23aea7e43879d3ce
etudiant <etudiant@debian>
2026-01-05 11:03:48 +0100

    Modification du display view server 2

--- spot_the_rt/server/controller/server_controller.py
+++ spot_the_rt/server/controller/server_controller.py
@@ -50,13 +50,43 @@ class ServerController:
                             thread.client_socket.send(new_message)
                             break
         
+        elif message.startswith("game"):
+            parts = message.split()
+            if len(parts) == 0:
+                return
+
+            sender = parts[0]
+
+            if "-create" in parts:
+                try:
+                    spot_the_RT_game.create_lobby_request(lobby_name_input=request[1], player_pseudo_input=request[2])
+
+            elif "-join" in parts:
+                try:
+                    spot_the_RT_game.join_lobby_request(lobby_name_input=request[1], player_pseudo_input=request[2])
+
+            elif "-launch" in parts:
+                try:
+                   spot_the_RT_game.launch_game_request(lobby_name_input=request[1])
+
+
+            elif "-quit" in parts:
+                try:
+                    spot_the_RT_game.quit_game_request(lobby_name_input=request[1], player_pseudo_input=request[2])
+
+
+            elif "-check" in parts:
+                try:
+                    spot_the_RT_game.is_symbol_correct_request(lobby_name_input=request[1], player_pseudo_input=request[2], symbol_input=request[3])
+                          
+
+        
         # message
         else:
             new_message = f"{client_username} : {message}"
             new_message = new_message.encode('utf-8')
             for thread in self.clients:
                     thread.client_socket.send(new_message)
-
 
         self.view.display_message(client_address, client_username, new_message)
 
--- spot_the_rt/server/view/console_view.py
+++ spot_the_rt/server/view/console_view.py
@@ -1,9 +1,45 @@
 class ConsoleView:
     def display_connection(self, client_address, client_username):
         print(f"[+] Client connecté : {client_username} ({client_address})")
+    
+    def display_username_error(self, client_address, client_username, room_number):
+        print(f"ERROR : Pseudo deja pris pour {client_address}")
 
     def display_message(self, client_address, client_username, message):
         print(f"{client_username} >> {message}")
 
     def display_disconnection(self, client_address, client_username):
         print(f"[-] Client déconnecté : {client_address} alias {client_username}")
+
+    ###
+
+    def display_room_create(self, client_address, client_username, room_number):
+        print(f"[+] Creation d'une salle par {client_username}@{client_address}, numéro {room_number}")
+
+    def display_room_delete(self, client_address, client_username, room_number):
+        print(f"[-] Supression d'une salle par {client_username}@{client_address}, numéro {room_number}")
+
+    ###
+
+    def display_room_join(self, client_address, client_username, room_number):
+        print(f"ROOM {room_number} : [+] Le joueur {client_username}@{client_address} a rejoins la room")
+
+    def display_room_leave(self, client_address, client_username, room_number):
+        print(f"ROOM {room_number} : [-] Le joueur {client_username}@{client_address} a quitté la room")
+
+    def display_room_join_error(self, client_address, client_username, room_number):
+        print(f"ERROR : Le joueur {client_username}@{client_address} a essayé de se connecter à la room {room_number}")
+
+    def display_room_launch(self, client_address, client_username, room_number):
+        print(f"ROOM {room_number} : Lancement d'une partie par {client_username}@{client_address}")
+
+    def display_room_launch_error(self, client_address, client_username, room_number):
+        print(f"ERROR : Pas assez de joueurs sur la room {room_number} pour le lancement")
+
+    ###  
+
+    def display_room_check_true(self, client_address, client_username, room_number):
+        print(f"ROOM {room_number} : [CHECK TRUE] Le joueur {client_username}@{client_address} a cliqué sur le bon symbole, BRAVO ")
+
+    def display_room_check_false(self, client_address, client_username, room_number):
+        print(f"ROOM {room_number} : [CHECK FALSE] Le joueur {client_username}@{client_address} a cliqué sur le mauvais symbole, DOMMAGE ")
